f9919a9f27cf5cd503bc25792627601f2b18a302
Kemmoun Ramzy <96486453+ramzy1453@users.noreply.github.com>
2024-01-14 08:54:23 +0100

    Update quranApi.js

--- src/libs/quranApi.js
+++ src/libs/quranApi.js
@@ -1,3 +1,12 @@
+async function getResponse(url: string, method = "GET", body: any = {}) {
+  const response = await fetch("http://api.alquran.cloud/v1" + url, {
+    method: method,
+    headers: { "Content-Type": "application/json" },
+    body: body,
+  });
+  return await response.json();
+}
+
 class QuranApi {
   async getMeta() {
     return await getResponse("/meta");
